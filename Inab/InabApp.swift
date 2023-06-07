8985cf21596840e04ed5e4c139664eb0ec067ca6
ACBS - nab <binhna@acbs.com.vn>
2023-06-07 09:43:59 +0700

    Initial Commit

---
+++ Inab/InabApp.swift
@@ -0,0 +1,17 @@
+//
+//  InabApp.swift
+//  Inab
+//
+//  Created by ACBS - nab on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct InabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
